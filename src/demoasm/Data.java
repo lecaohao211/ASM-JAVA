07cafc7006e360182ab01a10449b7a2b42098f92
lecaohao211 <102611398+lecaohao211@users.noreply.github.com>
2022-08-28 10:17:08 +0700

    up add

---
+++ src/demoasm/Data.java
@@ -0,0 +1,59 @@
+package demoasm;
+
+import java.util.ArrayList;
+
+public class Data {
+    private ArrayList<Teacher> list;
+    private String fileName;
+
+
+    public Data() {
+        this.list = new ArrayList<>();
+        this.fileName = "";
+    }
+
+    public ArrayList<Teacher> getList() {
+        return list;
+    }
+
+    public void setList(ArrayList<Teacher> list) {
+        this.list = list;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public void insert(Teacher teacher) {
+        list.add(teacher);
+    }
+
+    public void delete(Teacher teacher) {
+        int index =-1;
+        for(int i = 0; i<this.list.size(); i++) {
+            if(teacher.getId() == this.list.get(i).getId()) {
+                index = i;
+            }
+        }
+        this.list.remove(index);
+    }
+
+    public void update(Teacher student, int indexDelete) {
+        this.list.remove(indexDelete);
+        this.list.add(student);
+    }
+
+    public boolean checkIfExist(Teacher teacher) {
+        for (Teacher teacher1 : list) {
+            if(teacher1.getId() == teacher.getId()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
---
+++ src/demoasm/Teacher.java
@@ -0,0 +1,80 @@
+package demoasm;
+
+public class Teacher {
+    private int id;
+    private String name;
+    private int age;
+    private boolean gender;
+    private String subject;
+    private String position;
+    private String phone;
+
+    public Teacher() {
+    }
+
+    public Teacher(int id, String name, int age, boolean gender, String subject, String position, String phone) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.subject = subject;
+        this.position = position;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
